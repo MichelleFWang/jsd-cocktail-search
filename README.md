cd608ead6d6ed72fee071bba405221e5b76b5ae0
Michelle Wang <fengwang1995@gmail.com>
2023-08-17 16:57:36 +1000

    add favourites and css

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Cocktail Search
+# Home Cocktail Manual
 
+> Let's make cocktails!🍹
+
+Website Link: https://michellefwang.github.io/jsd-cocktail-search/
+
+Features:
 - A database of cocktail recipes
-- 
+- Search by cocktail name
+- Search by ingredients from provided list or enter your own
+- Add to favourites
+
+On the phone:
+
+<img src="./Images/Phone%20version.png" width=200/>
+
+Webpage:
+![image](./Images/Main%20page.png)
+
+Search by Ingredients:
+<img src="./Images/Search%20by%20ingredients.png"/>
+
+Details page:
+<img src="./Images/Details.png"/>
+
+
--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+//global
 const cocktailBaseUrl = 'https://www.thecocktaildb.com/api/json/v2/9973533/';
 
+//global selector nodes
 const resultsParent = document.querySelector( '#searchResults' );
 const searchFormNode = document.querySelector( '#searchForm' );
+const backButton = document.querySelector( '#backButton');
 const userSearchInput = document.querySelector( '#cocktailQuery' );
 const cocktailDetails = document.querySelector( '#cocktailDetails' );
 const ingredientsNode = document.querySelectorAll( '.ingredients' );
 const ingredientInput = document.querySelector( '#ingredientQuery' );
 const ingredientFormNode = document.querySelector( '#ingredientForm' );
 const chosenIngredientsNode = document.querySelector( '#chosen' );
-const searchTerms = []
+const favouriteButton = document.querySelector( '#favourite');
+const showFavourite = document.querySelector ( '#showFavourites');
+const modeBody = document.querySelector ( '#model-body' );
+const showFavouritesDetails = document.querySelector ( '#a' );
+const clearButton = document.querySelector ( '#clear' );
+//global arrays
+const searchTerms = [];
+const favourites = [];
 
+//search by ingredient form handler
 ingredientFormNode.addEventListener( 'submit', ev => {
     ev.preventDefault();
     searchByIngredients( ingredientInput.value );
@@ -19,7 +30,7 @@ ingredientFormNode.addEventListener( 'submit', ev => {
 });
 
 
-
+//search by clicking provided ingredients
 ingredientsNode.forEach( ingredient => {
     ingredient.addEventListener( 'click', ev => {
         console.log('clicked:' + ingredient.id );
@@ -29,13 +40,15 @@ ingredientsNode.forEach( ingredient => {
     })
 });
 
+//search by ingredients function and details page via id
 const searchByIngredients = (searchIngredients) => {
-    resultsParent.replaceChildren();
-
     console.log(searchIngredients);
-    searchTerms.push(searchIngredients);
-    
 
+    if(!searchTerms.includes(searchIngredients)){
+        searchTerms.push(searchIngredients);
+    }
+    resultsParent.replaceChildren();
+    
     axios.get(`https://www.thecocktaildb.com/api/json/v2/9973533/filter.php?i=${searchTerms}`)
     .then(res => {
         renderSearchResults( res.data.drinks );
@@ -68,12 +81,14 @@ const searchByIngredients = (searchIngredients) => {
     })
 }
 
+//search by name handler
 searchFormNode.addEventListener( 'submit', ev => {
     console.log( 'Form submitted!', userSearchInput.value );
     ev.preventDefault();
     loadSearchResults( userSearchInput.value );
 });
 
+//Load search results via cocktail name and details via id
 const loadSearchResults = (searchText) => {
 
     resultsParent.replaceChildren();
@@ -110,11 +125,19 @@ const loadSearchResults = (searchText) => {
         backButton.addEventListener('click', ev => { 
             resultsParent.style.display = "grid";
             cocktailDetails.style.display = "none";
-          })
+        });
 
 } //loadsearchresults()
 
+//clear button handler
+clearButton.addEventListener('click', ev => {
+    searchTerms.length=0;
+    chosenIngredientsNode.innerHTML = searchTerms;
+    // resultsParent.style.display = "grid";
+    // cocktailDetails.style.display = "none";
+})
 
+//render search results either from search by name or ingredients
 const renderSearchResults = (results) => {
 
     results.forEach( drinks => {
@@ -134,6 +157,20 @@ const renderSearchResults = (results) => {
     }); //foreach 
 }; // renderSearchResults()
 
+//show favourite button handler
+showFavourite.addEventListener ('click', ev =>{
+    showFavouritesDetails.innerHTML = favourites;
+})
+
+//push favourites into favourites array
+const favouritesPush = (data) => {
+    if(!favourites.includes(data)){
+        favourites.push(data);
+        console.log(favourites);
+    }   
+}
+
+//render the details of drinks
 const renderDetailsResults = (results) => {
 
     results.forEach( drinks => {
@@ -154,46 +191,79 @@ const renderDetailsResults = (results) => {
         cocktailDetails.appendChild( divTag );
 
         const pTag1 = document.createElement('p');
-        if(drinks.strIngredient1 != null){
+        if(drinks.strIngredient1 != null && drinks.strMeasure1 != null){
             pTag1.innerHTML = "Ingredients: " + drinks.strMeasure1 + " " + drinks.strIngredient1;
+        }
+        else if (drinks.strIngredient1 !=null && drinks.strMeasure1 == null) {
+            pTag1.innerHTML = "Ingredients: " + drinks.strIngredient1;
         }
         cocktailDetails.appendChild( pTag1 );
 
         const pTag2 = document.createElement('p');
-        if(drinks.strIngredient2 != null){
+        if(drinks.strIngredient2 != null && drinks.strMeasure2 != null){
             pTag2.innerHTML = drinks.strMeasure2 + " " + drinks.strIngredient2;
+        }
+        else if (drinks.strIngredient2 !=null && drinks.strMeasure2 == null) {
+            pTag2.innerHTML = drinks.strIngredient2;
         }
         cocktailDetails.appendChild( pTag2 );
 
         const pTag3 = document.createElement('p');
-        if(drinks.strIngredient3 != null){
+        if(drinks.strIngredient3 != null && drinks.strMeasure3 != null){
             pTag3.innerHTML = drinks.strMeasure3 + " " + drinks.strIngredient3;
+        }
+        else if (drinks.strIngredient3 !=null && drinks.strMeasure3 == null) {
+            pTag3.innerHTML = drinks.strIngredient3;
         }
         cocktailDetails.appendChild( pTag3 );
 
         const pTag4 = document.createElement('p');
-        if(drinks.strIngredient4 != null){
+        if(drinks.strIngredient4 != null && drinks.strMeasure4 != null){
             pTag4.innerHTML =  drinks.strMeasure4 + " " + drinks.strIngredient4;
+        }
+        else if (drinks.strIngredient4 !=null && drinks.strMeasure4 == null) {
+            pTag4.innerHTML = drinks.strIngredient4;
         }
         cocktailDetails.appendChild( pTag4);
 
         const pTag5 = document.createElement('p');
-        if(drinks.strIngredient5 != null){
+        if(drinks.strIngredient5 != null && drinks.strMeasure5 != null){
             pTag5.innerHTML = drinks.strMeasure5 + " " + drinks.strIngredient5;
+        }
+        else if (drinks.strIngredient5 !=null && drinks.strMeasure5 == null) {
+            pTag5.innerHTML = drinks.strIngredient5;
         }
         cocktailDetails.appendChild( pTag5 );
 
         const pTag6 = document.createElement('p');
-        if(drinks.strIngredient6 != null){
+        if(drinks.strIngredient6 != null && drinks.strMeasure6 != null){
             pTag6.innerHTML = drinks.strMeasure6 + " " + drinks.strIngredient6;
+        }
+        else if (drinks.strIngredient6 !=null && drinks.strMeasure6 == null) {
+            pTag6.innerHTML = drinks.strIngredient6;
         }
         cocktailDetails.appendChild( pTag6 );
 
-        const button = document.createElement('button');
-        button.innerText = 'favourite';
-        button.id = drinks.strDrink;
-        cocktailDetails.appendChild(button);
 
+        favouriteButton.style.display = 'flex';
+        favouriteButton.style.left = '47%';
+        favouriteButton.style.position = 'absolute';
+        favouriteButton.style.backgroundColor = '#FFCCCC';
+        favouriteButton.style.color = 'red';
+        favouriteButton.id = drinks.strDrink;
+        cocktailDetails.appendChild(favouriteButton);
+        
+        favouriteButton.addEventListener('click',ev =>{
+            ev.preventDefault();
+            favouritesPush(favouriteButton.id)
+            
+        })
+        
+        // const favouriteButton = document.createElement('button');
+        // favouriteButton.innerText = 'Add favourite';
+        // favouriteButton.id = drinks.strDrink;
+        // cocktailDetails.appendChild(favouriteButton);
 
     }); //foreach 
 }; // renderSearchResults()
+
